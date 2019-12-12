e109d70aea7eb1b4846726ff81af80d27239398d
Robert <xgarve@gmail.com>
2019-12-12 22:16:35 +0100

    better error handling refactoring threading

--- finanzen_net.py
+++ finanzen_net.py
@@ -1,32 +1,35 @@
 from collections import namedtuple
+from concurrent.futures import ThreadPoolExecutor
 
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import re
+import shelve
 
 
 class FinanzenNet:
-    def __init__(self, company):
+    def __init__(self, company, force_reload=False):
         self.company = company
-        self.dividend_soup = self.get_dividend_page()
-        self.stock_soup = self.get_stock_page()
-        self.fundamentals_soup = self.get_fundamentals_page()
 
-    def get_dividend_page(self):
-        page = requests.get(f'https://www.finanzen.net/dividende/{self.company}')
+        self.dividend_soup = self.get_page('dividende', force_reload)
+        self.stock_soup = self.get_page('aktien', force_reload)
+        self.fundamentals_soup = self.get_page('fundamentalanalyse', force_reload)
+
+    def get_page(self, website_category, force_reload):
+        with shelve.open('finanzen_net.shelve') as s:
+            saving_name = '/'.join([self.company, website_category])
+            if saving_name in s.keys() and not force_reload:
+                print(f'Loading {self.company}/{website_category} from disk...')
+                page = s[saving_name]
+            else:
+                print(f'Downloading {self.company}/{website_category} from finanzen.net...')
+                page = requests.get(f'https://www.finanzen.net/{website_category}/{self.company}')
+                s[saving_name] = page
         return BeautifulSoup(page.content, 'html.parser')
 
-    def get_stock_page(self):
-        page = requests.get(f'https://www.finanzen.net/aktien/{self.company}')
-        return BeautifulSoup(page.content, 'html.parser')
 
-    def get_fundamentals_page(self):
-        page = requests.get(f'https://www.finanzen.net/fundamentalanalyse/{self.company}')
-        return BeautifulSoup(page.content, 'html.parser')
-
-
-class Dividend:
+class DividendData:
     def __init__(self, finanzen_net):
         self.dividend_table = None
         self.company = finanzen_net.company
@@ -49,12 +52,19 @@ class Dividend:
             for dividend_information in self.dividend_soup.find_all('div', class_='table-responsive')[-2:]:
                 for tr in dividend_information.find_all('tr'):
                     tds = tr.find_all('td')
-                    if len(tds) > 4:
+                    try:
                         dividend = tds[2].get_text()
                         year = tds[4].get_text()
-                        if not dividend.startswith('*'):
-                            years.append(int(year))
-                            dividends.append(self.number_string_to_float(dividend.strip(), percentage=False))
+                        try:
+                            year = int(year)
+                            dividend = self.number_string_to_float(dividend.strip(), percentage=False)
+                        except ValueError:
+                            continue
+
+                        dividends.append(dividend)
+                        years.append(year)
+                    except IndexError:
+                        continue
 
             self.dividend_table = pd.Series(data=dividends, index=years)
             return self.dividend_table
@@ -62,36 +72,45 @@ class Dividend:
     def get_growth_rate(self, n_years=5):
         if self.dividend_table is None:
             self.get_dividends()
-        if len(self.dividend_table) > n_years:
-            if self.dividend_table.values[n_years] != 0:
-                return (self.dividend_table.values[0] / self.dividend_table.values[n_years]) ** (1 / n_years) - 1
-            else:
-                raise ZeroDivisionError(f'Dividend of {self.company} {n_years} years ago was zero.')
-        else:
-            raise IndexError(f'No dividend history for {self.company} {n_years} years ago.')
+        try:
+            return (self.dividend_table.values[0] / self.dividend_table.values[n_years]) ** (1 / n_years) - 1
+        except (ZeroDivisionError, IndexError):
+            return pd.np.nan
 
     def get_years_of_increase(self):
         if self.dividend_table is None:
             self.get_dividends()
-        s = self.dividend_table.diff()
-        now = s.index[0]
-        decrease = s[s > 0].index.max()
-        if decrease is not pd.np.nan:
-            return now - decrease - 1
-        else:
-            return len(self.dividend_table)
+        tmp = self.dividend_table.tolist()
+        try:
+            i = 0
+            while tmp[i] >= tmp[i + 1]:
+                i += 1
+            return i
+        except IndexError:
+            return len(tmp) if tmp else pd.np.nan
 
     def get_dividend_coverage(self):
-        res = namedtuple('DividendCoverage', 'earnings_growth dividend_yield payout_ratio')
+        res = namedtuple('DividendCoverage', 'earnings_growth forward_dividend_yield forward_payout_ratio')
 
         table = self.fundamentals_soup.find_all('div', class_='table-responsive')[-2]
         for tr in table.find_all('tr'):
             tds = tr.find_all('td')
             if tds[0].get_text() == 'langfristiges Wachstum':
-                res.earnings_growth = self.number_string_to_float(tds[1].get_text())
+                try:
+                    res.earnings_growth = self.number_string_to_float(tds[1].get_text())
+                except ValueError:
+                    res.earnings_growth = pd.np.nan
             elif tds[0].get_text() == 'Dividenden Rendite':
-                res.dividend_yield = self.number_string_to_float(tds[1].get_text())
-                res.payout_ratio = self.number_string_to_float(re.findall(' ([0-9,]+)%', tds[4].get_text())[0])
+                try:
+                    res.forward_dividend_yield = self.number_string_to_float(tds[1].get_text())
+                except ValueError:
+                    res.forward_dividend_yield = pd.np.nan
+
+                try:
+                    res.forward_payout_ratio = self.number_string_to_float(
+                        re.findall(' ([0-9,]+)%', tds[4].get_text())[0])
+                except (ValueError, IndexError):
+                    res.forward_payout_ratio = pd.np.nan
         return res
 
 
@@ -101,15 +120,43 @@ class Moodys:
         self.stock_soup = finanzen_net.stock_soup
 
     def get_credit_rating(self):
-        potential_credit_score = self.stock_soup.find('div', class_="tachoValue tachoMr mr4")
-        if potential_credit_score:
-            return potential_credit_score.get_text()
-        else:
-            raise LookupError(f'No credit score for {self.company}.')
+        try:
+            return self.stock_soup.find('div', class_="tachoValue tachoMr mr4").get_text()
+        except AttributeError:
+            return pd.np.nan
 
     def get_risk_score(self):
-        potential_credit_score = self.stock_soup.find('div', class_="tachoValue tachoMcrs mr1")
-        if potential_credit_score:
-            return potential_credit_score.get_text()
-        else:
-            raise LookupError(f'No risk score for {self.company}.')
+        try:
+            return self.stock_soup.find('div', class_="tachoValue tachoMcrs mr1").get_text()
+        except AttributeError:
+            return pd.np.nan
+
+
+class DividendSafetyReport:
+    def _assemble_single_company(self, company):
+        finanzen_net = FinanzenNet(company)
+        dividend = DividendData(finanzen_net)
+        moodys = Moodys(finanzen_net)
+
+        dividend_growth_rate = dividend.get_growth_rate()
+        dividend_increasing_years = dividend.get_years_of_increase()
+
+        coverage = dividend.get_dividend_coverage()
+        dividend_yield = coverage.forward_dividend_yield
+        earnings_growth = coverage.earnings_growth
+        payout_ratio = coverage.forward_payout_ratio
+
+        credit_rating = moodys.get_credit_rating()
+        risk_score = moodys.get_risk_score()
+
+        return pd.DataFrame(
+            [(company, dividend_growth_rate, dividend_increasing_years, dividend_yield, earnings_growth, payout_ratio,
+              credit_rating, risk_score)],
+            columns=['Name', 'Dividend Growth', 'Dividend Increase', 'Dividend Yield', 'Earning Growth', 'Payout Ratio',
+                     'Credit Rating', 'Risk Score']
+        )
+
+    def assemble_companies(self, companies):
+        with ThreadPoolExecutor(max_workers=4) as executor:
+            res = executor.map(self._assemble_single_company, companies)
+        return pd.concat(res)
--- main.py
+++ main.py
@@ -1,26 +1,5 @@
-from finanzen_net import FinanzenNet, Dividend, Moodys
+from finanzen_net import DividendSafetyReport
 
-finanzen_net = FinanzenNet('bayer')
-dividend = Dividend(finanzen_net)
-moodys = Moodys(finanzen_net)
-
-try:
-    print('Credit rating:', moodys.get_credit_rating())
-except LookupError as error:
-    print(error)
-
-try:
-    print('Risk score:', moodys.get_risk_score())
-except LookupError as error:
-    print(error)
-
-print('Dividend history:')
-print(dividend.get_dividends())
-
-n_years = 5
-print(f'Average annual dividend growth rate in the last {n_years} years was {dividend.get_growth_rate(n_years):.2%}')
-
-dividend_coverage = dividend.get_dividend_coverage()
-print(f'Dividend yield is {dividend_coverage.dividend_yield:.2%}. {dividend_coverage.payout_ratio:.2%} of the earnings are needed for covering this. The earning will grow by {dividend_coverage.earnings_growth:.2%} each year until 2021')
-
-print(f'There were {dividend.get_years_of_increase()} years of dividend non-decreases.')
+r = DividendSafetyReport()
+df = r.assemble_companies(['pepsico', '3m', 'bayer'])
+print()
